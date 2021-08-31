2abcec043ddff9d98853f460e16ceec48c7f1e83
Dave Mayo <pobocks@gmail.com>
2021-08-30 22:01:17 -0400

    Basic implementation of strawman ABC/parser registry, argparse group

--- mailbag/__init__.py
+++ mailbag/__init__.py
@@ -4,18 +4,27 @@
 __version__ = "0.0.1"
 
 
-from bagit import _make_parser
+from bagit import _make_parser, Bag
 from gooey import Gooey
+
+from .parser import EmailFormatParser
+
+print(EmailFormatParser.registry)
 
 bagit_parser = _make_parser()
 bagit_parser.description = f"Mailbag ({bagit_parser.description})"
+mailbagit_args = bagit_parser.add_argument_group("Mailbag")
+# add mailbag-specific args here
+mailbagit_args.add_argument("--foo", help="The foo argument, you know, that one")
 
 def cli():
     bagit_parser.parse_args()
+    # do the thing
 
 @Gooey
 def gui():
     bagit_parser.parse_args()
+    #do the thing
 
 class Mailbag:
     def __init__(self):
