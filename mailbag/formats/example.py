d7c300e79b7c890943ba00159870c53ba438c239
William David Mayo ("Dave") <pobocks@gmail.com>
2021-09-28 17:02:21 -0400

    Re-design of parsers to allow one-at-a-time message handling (#51)
    Due to the potential large size of email accounts, more messages can be
    present than can be handled at once in memory.  Thus, this version of
    the parser (renamed EmailAccount -> formats) separates per-account
    information and message-handling, and handles messages via a generator
    function so that they don't have to be returned to the caller in one
    giant chunk.
    
    Co-authored-by: gwiedeman <gregory.wiedeman1@gmail.com>

---
+++ mailbag/formats/example.py
@@ -0,0 +1,21 @@
+# This is an example parser, meant to show how
+# to hook up a real parser
+
+# Does nothing currently
+from mailbag.email_account import EmailAccount
+from mailbag.models import Email
+class ExampleAccount(EmailAccount):
+    format_name = 'example'
+
+    def __init__(self, target_account, **kwargs):
+        print("Parsity parse")
+        # code goes here to set up mailbox and pull out any relevant account_data
+        account_data = {}
+        messages = []
+
+    def account_data(self):
+        return account_data
+
+    def messages(self):
+        for message in messages:
+            yield Email(**message)
