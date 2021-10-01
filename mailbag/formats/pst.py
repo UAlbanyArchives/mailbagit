d78ee8f0e3ece2ec80f0ff112d610c3e1335b15d
haritgarg <haritgarg1997@gmail.com>
2021-10-01 00:48:06 -0400

    Added arguments mentioned in CLI arguments working doc

---
+++ mailbag/formats/pst.py
@@ -0,0 +1,57 @@
+from mailbag.email_account import EmailAccount
+from mailbag.models import Email
+import mailbox
+import pypff
+
+
+class PST(EmailAccount):
+    # pst - This concrete class parses PST file format
+    format_name = 'pst'
+
+    def __init__(self, target_account, **kwargs):
+        print("Parsity parse")
+        # code goes here to set up mailbox and pull out any relevant account_data
+
+
+        self.file =target_account.input_path
+        print("Reading :", self.file)
+        pst = pypff.file()
+        pst.open("self.file")
+
+        self.data = pst.get_root_folder()
+
+    def account_data(self):
+        return account_data
+
+    def messages(self):
+        for folder in self.sub_folders:
+
+                messages1 = []
+                if folder.number_of_sub_folders:
+                    messages1 += PST.messages(self=folder)
+                for msg in folder.sub_messages:
+                    message = {
+                    #"Message_ID":msg.id,
+                    "Email_Folder":msg.folder.name,
+                    #"Date":msg.date,
+                    "From":msg.sender_name,
+                    #"To":msg.to,
+                    #Cc":msg.cc,
+                    #"Bcc":msg.bcc,
+                    "Subject":msg.subject,
+                    #"Content_Type":msg.content_type
+
+
+                         }
+
+
+
+        yield Email(**message)
+
+
+
+
+
+
+
+
