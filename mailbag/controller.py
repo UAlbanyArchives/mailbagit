df0f132dcfa82cd8d4f4d479bd45f501e6cac3a1
baibhav <baibhav@bob-Nitro-AN515-54>
2021-10-01 22:20:26 -0400

    format_map replaced with registry property

--- mailbag/controller.py
+++ mailbag/controller.py
@@ -1,15 +1,16 @@
-
+from mailbag.email_account import EmailAccount
+from dataclasses import dataclass, asdict, field, InitVar
 
 class Controller:
     """Controller - Main controller"""
     
     def __init__(self, args, formats):
         self.args = args
-        self.format_map = {
-                'mbox': formats.mbox.Mbox,
-                'msg': formats.msg.MSG
-        }
-        
+    
+    @property
+    def format_map(self):
+        return EmailAccount.registry
+
     def read(self):
 
         format = self.format_map[self.args.input](self.args)            
