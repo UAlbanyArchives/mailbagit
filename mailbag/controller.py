2bab123075a0ea10e7c0af1e4808e430cf0f2570
gwiedeman <gregory.wiedeman1@gmail.com>
2021-10-04 13:08:54 -0400

    merged new arguments with controller and format parsers

--- mailbag/controller.py
+++ mailbag/controller.py
@@ -13,8 +13,15 @@ class Controller:
 
     def read(self):
 
-        format = self.format_map[self.args.input](self.args)            
-        messages = format.messages()
+        if len(self.args.directory) > 1:
+            # checks that mailbag was only given one directory as input. 
+            # bagit-python loops through all directory args, and we may have to 
+            # handle multiple inputs at some point but for now just raise an error.
+            raise ValueError("Mailbag currently only reads one input source.")
+        else:
+
+            format = self.format_map[self.args.input](self.args.directory[0])            
+            messages = format.messages()
         
         for m in messages:
             print(m.to_struct())
