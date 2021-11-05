6e6588b55a4df722d310f7134d0ee2e46eb394a9
haritgarg <89418371+haritgarg@users.noreply.github.com>
2021-11-05 01:25:54 -0400

    PYTEST for PST (#67)
    * PYTEST for PST
    
    * PST Pytest and Global path
    
    * PST Pytest and Global path v2
    
    * made Email_Folder path attribure cross-platform
    
    Co-authored-by: gwiedeman <gregory.wiedeman1@gmail.com>

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,13 +1,13 @@
-
 from mailbag.controller import Controller
 from mailbag.models import Email
 import mailbag
 import pytest
+import os
 
 
 def test_Mbox():
-    data = mailbag.formats.mbox.Mbox("data/sample1.mbox").messages()
-    
+    data = mailbag.formats.mbox.Mbox(os.path.join("data", "sample1.mbox")).messages()
+
     expected = []
     expected.append(Email(
         Content_Type='multipart/alternative; \n\tboundary="----=_Part_315462_994415758.1467289309709"',
@@ -16,7 +16,7 @@ def test_Mbox():
         Message_ID='<1314145029.920292481467289359777.JavaMail.app@rbg21.atlis1>',
         Subject='=?utf-8?Q?The_GOP=E2=80=99s_horrendous_damage?=',
         To='ualbanymodernpoliticalarchives@gmail.com'
-    )) 
+    ))
     expected.append(Email(
         Content_Type='multipart/alternative; boundary=94eb2c123e4c3234a50535f25ea8',
         Date='Thu, 23 Jun 2016 13:52:42 +0000',
@@ -31,7 +31,7 @@ def test_Mbox():
 
 
 def test_MSG():
-    data = mailbag.formats.msg.MSG("data/sample1.msg").messages()
+    data = mailbag.formats.msg.MSG(os.path.join("data", "sample1.msg")).messages()
 
     expected = []
     expected.append(Email(
@@ -43,3 +43,23 @@ def test_MSG():
 
     for id, m in enumerate(data):
         assert m == expected[id]
+
+
+def test_PST():
+    data = mailbag.formats.pst.PST(os.path.join("data", "outlook2019_MSO_16.0.10377.20023_64-bit.pst")).messages()
+
+    expected = []
+    expected.append(Email(
+        Cc='gwiedeman@albany.edu',
+        Content_Type='multipart/alternative; boundary=da8640888b204494a76c3a9e1f2a9112f911e113df090efab9b04e884486',
+        Date='Thu, 30 Sep 2021 17:30:56 +0000 (UTC)',
+        Email_Folder=os.path.join('Top of Outlook data file', 'Inbox', 'Today at UAlbany'),
+        From='Today at UAlbany <tau@albany.edu>',
+        Message_ID='<2Y2JQkdFSGmQAIdjAOswdQ@geopod-ismtpd-3-0>',
+        Subject='Today at UAlbany - Focus on Research',
+        To='gwiedeman@albany.edu'
+    ))
+
+    for id, m in enumerate(data):
+        assert m == expected[id]
+        break
