74bf1f9c4d4759189cecb7a2043ca671166f0b0f
baibhav <baibhav@bob-Nitro-AN515-54>
2022-02-11 09:58:57 -0500

    Now eml parser stores attachments to email model

--- mailbag/formats/eml.py
+++ mailbag/formats/eml.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 from os.path import join
-#import mailbag.helper as helper
+import mailbag.helper as helper
 import mailbox
 from structlog import get_logger
 from email import parser
@@ -9,7 +9,7 @@ from mailbag.email_account import EmailAccount
 from mailbag.models import Email
 import email
 import glob, os
-
+from email import policy
 
 
 
@@ -22,7 +22,7 @@ class EML(EmailAccount):
     """EML - This concrete class parses eml file format"""
     format_name = 'eml'
 
-    def __init__(self, target_account, **kwargs):
+    def __init__(self, target_account, args, **kwargs):
         log.debug("Parsity parse")
         # code goes here to set up mailbox and pull out any relevant account_data
         account_data = {}
@@ -38,15 +38,22 @@ class EML(EmailAccount):
 
     def messages(self):
 
-        files = glob.glob(os.path.join(self.file, "**", "*.eml"), recursive=True)
+        # files = glob.glob(os.path.join(self.file, "**", "*.eml"), recursive=True)
+        files = ["data/sample1_jpg_attachment.eml"]
 
         for i in files:
             #log.debug(i)
+            attachmentNames = []
+            attachments = []
             with open(i, 'r') as f:
-                #a = f.read()
-                msg = email.message_from_file(f)
-
-
+                msg = email.message_from_file(f, policy=policy.default)
+                
+                for attachment in msg.iter_attachments():
+                    
+                    attachmentName,attachment = helper.saveAttachments(attachment)
+                    if attachmentName:
+                        attachmentNames.append(attachmentName)
+                        attachments.append(attachment)
 
             message = Email(
                     #Email_Folder=helper.emailFolder(self.dry_run, self.mailbag_name, self.format_name, self.file,i),
@@ -55,7 +62,10 @@ class EML(EmailAccount):
                     From=msg["from"],
                     To=msg["to"],
                     Subject=msg["subject"],
-                    Content_Type=msg["content-type"]
+                    Content_Type=msg["content-type"],
+                    AttachmentNum=len(attachmentNames),
+                    AttachmentNames=attachmentNames,
+                    AttachmentFiles=attachments
                 )
 
 
