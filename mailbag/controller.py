2295855d310afcf3a98f368c797a0d72c6c04bcc
Harit <haritgarg1997@gmail.com>
2022-02-18 13:19:18 -0500

    csv-with splitting files according to specification

--- mailbag/controller.py
+++ mailbag/controller.py
@@ -7,8 +7,8 @@ from pathlib import Path
 import os, shutil, glob
 import mailbag.helper as helper
 
-
 log = get_logger()
+
 
 class Controller:
     """Controller - Main controller"""
@@ -27,33 +27,63 @@ class Controller:
         return Derivative.registry
 
     def generate_mailbag(self):
-        mail_account : EmailAccount = self.format(self.args.directory, self.args)
+        mail_account: EmailAccount = self.format(self.args.directory, self.args)
 
         derivatives = [d(mail_account) for d in self.derivatives_to_create]
 
         # do stuff you ought to do with per-account info here
         # mail_account.account_data()
-        #for d in derivatives:
-        #    d.do_task_per_account()
+        # for d in derivatives:
+        # d.do_task_per_account()
         os.mkdir(os.path.join(self.args.directory, self.args.mailbag_name))
         files = os.path.join(self.args.directory, self.args.mailbag_name, "mailbag.csv")
         header = ['Mailbag-Message-ID', 'Message_ID', 'Email_Folder', 'Date', 'From', 'To', 'Cc', 'Bcc', 'Subject',
                   'Content_Type', 'Error']
-        with open(files, 'w', encoding='UTF8', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(header)
-            mailbag_message_id = 1
-            '''mailbag_message_id Variable to declare Mailbag_Message_ID field in csv'''
-            for message in mail_account.messages():
-                # do stuff you ought to do per message here
-                writer.writerow(
-                    [mailbag_message_id, message.Message_ID, message.Email_Folder, message.Date, message.From, message.To, message.Cc,
-                     message.Bcc, message.Subject, message.Content_Type, message.Error])
-                mailbag_message_id = mailbag_message_id + 1
-
+        csv_data = []
+        mailbag_message_id = 0
+        csv_portion_count = 0
+        csv_portion = []
+        csv_portion.append(header)
         for message in mail_account.messages():
             # do stuff you ought to do per message here
+            if csv_portion_count > 100000:
+                mailbag_message_id = 1
+                message.Mailbag_Message_ID = mailbag_message_id
+                csv_data.append(csv_portion)
+                csv_portion = []
+                csv_portion.append(header)
+                csv_portion.append(
+                    [message.Mailbag_Message_ID, message.Message_ID, message.Email_Folder, message.Date, message.From,
+                     message.To, message.Cc,message.Bcc, message.Subject, message.Content_Type, message.Error])
+                csv_portion_count = 0
+            else:
+                mailbag_message_id += 1
+                message.Mailbag_Message_ID = mailbag_message_id
+                csv_portion.append(
+                    [message.Mailbag_Message_ID, message.Message_ID, message.Email_Folder, message.Date, message.From,
+                     message.To, message.Cc,message.Bcc, message.Subject, message.Content_Type, message.Error])
+            csv_portion_count += 1
+
+        csv_data.append(csv_portion)
+        #if not self.args.dry_run:
+
+        if len(csv_data) == 1:
+
+                with open("mailbag.csv", 'w', encoding='UTF8', newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerows(csv_data)
+        else:
+                portion_count = 0
+                for portion in csv_data:
+                    portion_count += 1
+                    filename = "mailbag-" + str(portion_count) + ".csv"
+                    with open(filename, 'w', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerows(portion)
+
+        for message in mail_account.messages():
+        # do stuff you ought to do per message here
             for d in derivatives:
-                d.do_task_per_message(message)
+                        d.do_task_per_message(message)
 
         return mail_account.messages()
