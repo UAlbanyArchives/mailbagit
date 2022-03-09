8c1297ce32f2182e5e9436fd386210a2af8897c3
gwiedeman <gregory.wiedeman1@gmail.com>
2022-03-09 14:02:07 -0500

    now appends table after <body> tag and falls back to Text body if no html

--- mailbag/derivatives/pdf.py
+++ mailbag/derivatives/pdf.py
@@ -16,36 +16,65 @@ class ExampleDerivative(Derivative):
 
     def do_task_per_message(self, message, args):
         wkhtmltopdf = 'wkhtmltopdf.exe'
-        html = message.HTML_Body
-        table="<table>"
-        headerFields=[]
-        #Getting all the required attributes of message except error and body
-        for attribute in message:
-            if(attribute[0] not in ("Error","Text_Body","HTML_Body","Message","Headers") ):
-                headerFields.append(attribute[0])
-        #Getting the values of the attrbutes and appending to HTML string
-        for headerField in headerFields:
-            if not getattr(message,headerField) is None:
-                table= table+"<tr>"
-                table= table+"<td>"+str(headerField)+"</td>"
-                table= table+"<td>"+str(getattr(message,headerField))+"</td>"
-                table= table+"</tr>"
-        table=table+"</table>"
-        table=table+html
-        html_name = os.path.join(args.directory, args.mailbag_name,str(message.Mailbag_Message_ID )+".html")
-        pdf_name = os.path.join(args.directory, args.mailbag_name,str(message.Mailbag_Message_ID )+".pdf")
-        log.debug("Writing HTML to " + str(html_name))
-        if not args.dry_run:
-            write_html = open(html_name, 'w')
-            write_html.write(table)
-            write_html.close()
-        p = subprocess.Popen([wkhtmltopdf, html_name, pdf_name],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        stdout, stderr=p.communicate()
-        if stdout:
-            log.debug(stdout)
-        if stderr:
-            log.debug("Error while creating pdf")
-            raise TypeError(stderr)
+        
+        #check to see which body to use
+        body = False
+        if message.HTML_Body:
+            body = message.HTML_Body
+        elif message.Text_Body:
+            body = message.Text_Body
+        else:
+            log.debug("Unable to create PDF, no body found for " + str(message.Mailbag_Message_ID))
+
+        if body:
+            table="<table>"
+            headerFields=[]
+            #Getting all the required attributes of message except error and body
+            for attribute in message:
+                if(attribute[0] not in ("Error","Text_Body","HTML_Body","Message","Headers") ):
+                    headerFields.append(attribute[0])
+            #Getting the values of the attrbutes and appending to HTML string
+            for headerField in headerFields:
+                if not getattr(message,headerField) is None:
+                    table += "<tr>"
+                    table += "<td>"+str(headerField)+"</td>"
+                    table += "<td>"+str(getattr(message,headerField))+"</td>"
+                    table += "</tr>"
+            table += "</table>"
+
+            #add headers table to html
+            if message.HTML_Body and "<body" in body.lower():
+                body_position = body.lower().index("<body")
+                table_position = body_position + body[body_position:].index(">") + 1
+                html_content = body[:table_position] + table + body[table_position:]
+            else:
+                #fallback to just prepending the table
+                html_content = table + body
+
+            pdf_path = os.path.join(args.directory, args.mailbag_name, "pdf")
+            if not os.path.isdir(pdf_path):
+                os.mkdir(pdf_path)
+            html_name = os.path.join(pdf_path, str(message.Mailbag_Message_ID )+".html")
+            pdf_name = os.path.join(pdf_path, str(message.Mailbag_Message_ID )+".pdf")
+            log.debug("Writing HTML to " + str(html_name) + " and converting to " + str(pdf_name))
+            if not args.dry_run:
+                write_html = open(html_name, 'w')
+                write_html.write(html_content)
+                write_html.close()
+                p = subprocess.Popen([wkhtmltopdf, html_name, pdf_name],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                stdout, stderr = p.communicate()
+                if p.returncode == 0:
+                    log.debug("Successfully created " + str(message.Mailbag_Message_ID )+".pdf")
+                else:
+                    if stdout:
+                        log.error("Error creating " + str(message.Mailbag_Message_ID )+".pdf: " + str(stdout))
+                    if stderr:
+                        log.error("Error creating " + str(message.Mailbag_Message_ID )+".pdf: " + str(stderr))
+                    #comment out for now since catches even very minor issues
+                    #raise TypeError(stderr)
+                # delete the HTML file
+                os.remove(html_name)
+
 
 
 
