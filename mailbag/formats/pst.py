bd79050782049c3a7715829bb5c6e6448730ccbb
gwiedeman <gregory.wiedeman1@gmail.com>
2022-04-28 15:52:12 -0400

    now handles attachment filename encoding and reads mime types right

--- mailbag/formats/pst.py
+++ mailbag/formats/pst.py
@@ -62,9 +62,7 @@ if not skip_registry:
 
                         try:
                             headerParser = parser.HeaderParser()
-                            headers = headerParser.parsestr(
-                                messageObj.transport_headers
-                            )
+                            headers = headerParser.parsestr(messageObj.transport_headers)
                         except Exception as e:
                             desc = "Error parsing message body"
                             errors = helper.handle_error(errors, e, desc)
@@ -80,54 +78,45 @@ if not skip_registry:
                             # Docs say to use MESSAGE_CODEPAGE: https://github.com/libyal/libfmapi/blob/main/documentation/MAPI%20definitions.asciidoc#51-the-message-body
                             # this is a 32bit encoded integer
                             encodings = {}
-                            LIBPFF_ENTRY_TYPE_MESSAGE_BODY_CODEPAGE = int(
-                                "0x3fde", base=16
-                            )
+                            LIBPFF_ENTRY_TYPE_MESSAGE_BODY_CODEPAGE = int("0x3fde", base=16)
                             LIBPFF_ENTRY_TYPE_MESSAGE_CODEPAGE = int("0x3ffd", base=16)
                             for record_set in messageObj.record_sets:
                                 for entry in record_set.entries:
-                                    if (
-                                        entry.entry_type
-                                        == LIBPFF_ENTRY_TYPE_MESSAGE_BODY_CODEPAGE
-                                    ):
+                                    if entry.entry_type == LIBPFF_ENTRY_TYPE_MESSAGE_BODY_CODEPAGE:
                                         if entry.data:
                                             value = struct.unpack("i", entry.data)[0]
                                             # Use the extract_msg code page in constants.py
-                                            encodings[
-                                                "PidTagInternetCodepage"
-                                            ] = CODE_PAGES[value]
-                                    if (
-                                        entry.entry_type
-                                        == LIBPFF_ENTRY_TYPE_MESSAGE_CODEPAGE
-                                    ):
+                                            encodings["PidTagInternetCodepage"] = CODE_PAGES[value]
+                                    if entry.entry_type == LIBPFF_ENTRY_TYPE_MESSAGE_CODEPAGE:
                                         if entry.data:
                                             value = struct.unpack("i", entry.data)[0]
                                             # Use the extract_msg code page in constants.py
-                                            encodings[
-                                                "PidTagMessageCodepage"
-                                            ] = CODE_PAGES[value]
+                                            encodings["PidTagMessageCodepage"] = CODE_PAGES[value]
 
                             if messageObj.html_body:
-                                html_encoding = chardet.detect(messageObj.html_body)[
-                                    "encoding"
-                                ]
+                                if encodings["PidTagInternetCodepage"]:
+                                    html_encoding = encodings["PidTagInternetCodepage"]
+                                elif encodings["PidTagMessageCodepage"]:
+                                    html_encoding = encodings["PidTagMessageCodepage"]
+                                else:
+                                    html_encoding = chardet.detect(messageObj.html_body)["encoding"]
                                 html_body = messageObj.html_body.decode(html_encoding)
                             if messageObj.plain_text_body:
-                                text_encoding = chardet.detect(
-                                    messageObj.plain_text_body
-                                )["encoding"]
-                                text_body = messageObj.plain_text_body.decode(
-                                    text_encoding
-                                )
+                                if encodings["PidTagInternetCodepage"]:
+                                    text_encoding = encodings["PidTagInternetCodepage"]
+                                elif encodings["PidTagMessageCodepage"]:
+                                    text_encoding = encodings["PidTagMessageCodepage"]
+                                else:
+                                    text_encoding = chardet.detect(messageObj.plain_text_body)["encoding"]
+                                text_encoding = chardet.detect(messageObj.plain_text_body)["encoding"]
+                                text_body = messageObj.plain_text_body.decode(text_encoding)
                         except Exception as e:
                             desc = "Error parsing message body"
                             errors = helper.handle_error(errors, e, desc)
 
                         # Build message and derivatives paths
                         try:
-                            messagePath = os.path.join(
-                                os.path.splitext(originalFile)[0], *path
-                            )
+                            messagePath = os.path.join(os.path.splitext(originalFile)[0], *path)
                             if len(messagePath) > 0:
                                 messagePath = Path(messagePath).as_posix()
                             derivativesPath = helper.normalizePath(messagePath)
@@ -138,51 +127,42 @@ if not skip_registry:
                         try:
                             total_attachment_size_bytes = 0
                             for attachmentObj in messageObj.attachments:
-                                total_attachment_size_bytes = (
-                                    total_attachment_size_bytes
-                                    + attachmentObj.get_size()
-                                )
-                                attachment_content = attachmentObj.read_buffer(
-                                    attachmentObj.get_size()
-                                )
+                                total_attachment_size_bytes = total_attachment_size_bytes + attachmentObj.get_size()
+                                attachment_content = attachmentObj.read_buffer(attachmentObj.get_size())
 
                                 try:
                                     # attachmentName = attachmentObj.get_name()
                                     # Entries found here: https://github.com/libyal/libpff/blob/main/libpff/libpff_mapi.h#L333-L335
-                                    LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_LONG = int(
-                                        "0x3707", base=16
-                                    )
-                                    LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_SHORT = int(
-                                        "0x3704", base=16
-                                    )
-                                    attachmentName = ""
+                                    LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_LONG = int("0x3707", base=16)
+                                    LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_SHORT = int("0x3704", base=16)
+                                    LIBPFF_ENTRY_TYPE_ATTACHMENT_MIME_TAG = int("0x370e", base=16)
+                                    attachmentLong = ""
+                                    attachmentShort = ""
+                                    mime = None
                                     for record_set in attachmentObj.record_sets:
                                         for entry in record_set.entries:
-                                            if (
-                                                entry.entry_type
-                                                == LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_LONG
-                                            ):
+                                            if entry.entry_type == LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_LONG:
                                                 if entry.data:
-                                                    att_enc = chardet.detect(
-                                                        entry.data
-                                                    )["encoding"]
-                                                    attachmentName = entry.data.decode(
-                                                        att_enc
-                                                    ).replace(chr(0), "")
-                                            if (
-                                                entry.entry_type
-                                                == LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_SHORT
-                                            ):
-                                                if (
-                                                    entry.data
-                                                    and len(attachmentName) > 0
-                                                ):
-                                                    att_enc = chardet.detect(
-                                                        entry.data
-                                                    )["encoding"]
-                                                    attachmentName = entry.data.decode(
-                                                        att_enc
-                                                    ).replace(chr(0), "")
+                                                    attachmentLong = entry.get_data_as_string()
+                                            if entry.entry_type == LIBPFF_ENTRY_TYPE_ATTACHMENT_FILENAME_SHORT:
+                                                if entry.data:
+                                                    attachmentShort = entry.get_data_as_string()
+                                            if entry.entry_type == LIBPFF_ENTRY_TYPE_ATTACHMENT_MIME_TAG:
+                                                if entry.data:
+                                                    mime = entry.get_data_as_string()
+                                    # Use the Long filename preferably
+                                    if len(attachmentLong) > 0:
+                                        attachmentName = attachmentLong
+                                    elif len(attachmentShort) > 0:
+                                        attachmentName = attachmentShort
+                                    else:
+                                        print(message.Mailbag_Message_ID)
+                                        raise ValueError("No attachment name found.")
+
+                                    # Guess the mime if we can't find it
+                                    if mime is None:
+                                        mime = helper.guessMimeType(attachmentName)
+
                                 except Exception as e:
                                     attachmentName = str(len(attachments))
                                     desc = (
@@ -196,7 +176,7 @@ if not skip_registry:
                                 attachment = Attachment(
                                     Name=attachmentName,
                                     File=attachment_content,
-                                    MimeType=helper.guessMimeType(attachmentName),
+                                    MimeType=mime,
                                 )
                                 attachments.append(attachment)
 
@@ -230,9 +210,7 @@ if not skip_registry:
                     except (Exception) as e:
                         desc = "Error parsing message"
                         errors = helper.handle_error(errors, e, desc)
-                        message = Email(
-                            Error=errors["msg"], StackTrace=errors["stack_trace"]
-                        )
+                        message = Email(Error=errors["msg"], StackTrace=errors["stack_trace"])
 
                     yield message
 
@@ -267,9 +245,7 @@ if not skip_registry:
                 for folder in root.sub_folders:
                     if folder.number_of_sub_folders:
                         # call recursive function to parse email folder
-                        yield from self.folders(
-                            folder, pathList, os.path.basename(filePath)
-                        )
+                        yield from self.folders(folder, pathList, os.path.basename(filePath))
                     else:
                         # gotta return empty directory to controller somehow
                         log.error("???--> " + folder.name)
