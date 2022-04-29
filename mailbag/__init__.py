f200bc939769adb72ade1352804732bc9dfc9e1f
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2022-04-29 16:43:11 -0400

    Fix GUI args 76 (#147)
    * Ignore project setting files
    
    * Attachment model added to store list of attachments
    
    * MimeType of attachments added to model for eml,mbox,msg, and pst formats
    
    * Walk extract attachment replaces iter_attachment for mbox and eml
    
    * helper method for html formatting and pdf derivative
    
    * make parsing EML and MBOX parts into a helper, also made error handling into a helper. Minor attachement fixes as well
    
    * also caught this old error code which would have failed
    
    * cleaned up encoding and variable names and removed some excess code
    
    * changed sample data
    
    * msg and eml tests now running, object compare still not working
    
    * added dump and read methods to model and script to dump messages
    
    * worked on tests, they pass now but don't compare the header and message objects
    
    * now tests headers too
    
    * removed test load file
    
    * refactored pickle load to hopefully prevent truncating
    
    * dumping the data during tests to see if that fixes the issue
    
    * --
    
    * fixed encoding issue, added meta tag with beautiful soup,
    adding table to html when pdf derivative is parsed
    
    * refactored a big and adjusted headers table to be more usable
    
    * dump now writes LF endings and .gitattributes changed to hopefully not corrupt same data files
    
    * hopefully this will leave binary dump files as-is
    
    * dump files now have appropriate extentsions and git should stop messing with line endings in sample data
    
    * tests now match correct message with dump file and correct attachments
    
    * changed how the build installs dependancies which should make tests work
    
    * dump files should be the same across os platforms now
    
    * fixed bad merge and handled some bugs
    
    * added bs4 requirement
    
    * implemented bag-info metadata according to the spec, also fixed mbox derivative path bug
    
    * added black dependency for earlier python versions
    
    * reformtted with black
    
    * removing black on build as it doesn't do anything that way
    
    * rebased with fix_tests-91
    
    * now reads attachment filenames from entries without getFilename method so we can use base libpff
    
    * updated build so tests will pass
    
    * GUI can now take multiple derivatives values. Still breaking when you run it though.
    
    * works from .py file however
    
    * implemented derivatives error handling for txt. also added pyproject.toml to repo for black config
    
    * added to handle_error docs
    
    * Adds error handling for EML, HTML, PDF, and TXT derivatives
    
    * error handling for mbox and warc derivatives, warc server now closes on ctrl+c
    
    * started managing bagit args for better or worse
    
    * bagit args now handled correctly and Gooey GUI formatted correctly
    
    * updated field help descriptions
    
    * now raises an error if source format is included in derivatives
    
    * renamed gui .py file and switch directory back to text box
    
    * changed gui loader for consistancy and removed dir selector
    
    Co-authored-by: baibhav <baibhav@bob-Nitro-AN515-54>
    Co-authored-by: Harit <haritgarg1997@gmail.com>

--- mailbag/__init__.py
+++ mailbag/__init__.py
@@ -5,9 +5,10 @@ __version__ = "0.0.1"
 
 import os
 from pathlib import Path
-from bagit import _make_parser, Bag
-from gooey import Gooey
+from bagit import _make_parser, Bag, BagHeaderAction, DEFAULT_CHECKSUMS
+from gooey import Gooey, GooeyParser
 from structlog import get_logger
+from argparse import ArgumentParser
 from mailbag.email_account import EmailAccount, import_formats
 from mailbag.derivative import Derivative, import_derivatives
 from mailbag.controller import Controller
@@ -35,17 +36,91 @@ log.debug("EmailAccount:", Registry=EmailAccount.registry)
 log.debug("Derivative:", Registry=Derivative.registry)
 
 bagit_parser = _make_parser()
-bagit_parser.description = f"Mailbag ({bagit_parser.description})"
-mailbagit_args = bagit_parser.add_argument_group("Mailbag arguments")
-mailbagit_options = bagit_parser.add_argument_group("Mailbag options")
-mailbagit_metadata = bagit_parser.add_argument_group("Optional Mailbag Metadata")
+mailbag_parser = GooeyParser(description="Mailbag")
+mailbag_parser.set_defaults(bag_info={})
+# print(dir(bagit_parser))
+# bagit_parser.description = f"Mailbag ({bagit_parser.description})"
+mailbagit_args = mailbag_parser.add_argument_group("Mailbag arguments")
+mailbagit_options = mailbag_parser.add_argument_group("Mailbag options")
+mailbagit_metadata = mailbag_parser.add_argument_group("Optional Mailbag Metadata")
 
-input_types = list(EmailAccount.registry.keys())
-derivative_types = list(Derivative.registry.keys())
+# Load relevant args from bagit_parser to mailbag_parser
+# This is necessary as mailbagit does not support --validate, --fast, or --completeness-only
+# Checksum args also do not action="store_true" so they don't display as checkboxes with Gooey
+# Excluding log and quiet for not since we're not handling these yet
+exclude_args = ["directory", "help", "validate", "fast", "completeness_only", "log", "quiet"]
+for arg_group in bagit_parser._action_groups:
+    # print(arg_group.__dict__["title"].title())
+    group = mailbag_parser.add_argument_group(arg_group.__dict__["title"].title())
+    group.description = arg_group.__dict__["description"]
+    for i, action in enumerate(arg_group._actions):
+        if action.container == arg_group and not action.dest in exclude_args:
+            # print ("\t" + action.dest)
+            if action.nargs == 0:
+                # checksum options
+                group.add_argument(
+                    action.option_strings[0],
+                    required=action.required,
+                    default=action.default,
+                    help=action.help,
+                    dest=action.dest,
+                    const=action.const,
+                    action="append_const",
+                    widget="BlockCheckbox",
+                )
+            else:
+                if arg_group.__dict__["title"].lower() == "optional bag metadata":
+                    group.add_argument(
+                        action.option_strings[0],
+                        required=action.required,
+                        default=action.default,
+                        help=action.help,
+                        type=str,
+                        dest=action.dest,
+                        action=BagHeaderAction,
+                        nargs=action.nargs,
+                    )
+                elif action.dest.lower() == "processes":
+                    group.add_argument(
+                        action.option_strings[0],
+                        required=action.required,
+                        default=action.default,
+                        type=int,
+                        help=action.help,
+                        choices=action.choices,
+                        dest=action.dest,
+                        nargs=action.nargs,
+                    )
+                else:
+                    group.add_argument(
+                        action.option_strings[0],
+                        required=action.required,
+                        default=action.default,
+                        help=action.help,
+                        choices=action.choices,
+                        dest=action.dest,
+                        nargs=action.nargs,
+                    )
+
+
+input_types = list(key for key in EmailAccount.registry.keys() if key != "example")
+derivative_types = list(key for key in Derivative.registry.keys() if key != "example")
+
+# Path arg to override bagit directory arg
+mailbagit_args.add_argument(
+    "directory",
+    nargs=1,
+    # widget="DirChooser",
+    help=(
+        "A path to email to be packaged into a mailbag." "This can be a single file or a directory containing " "a number of email exports."
+    ),
+)
+
 
 # add mailbag-specific required args here
+mailbagit_args.add_argument("-m", "--mailbag_name", required=True, help="A directory name for the mailbag root directory.", nargs=None)
 mailbagit_args.add_argument(
-    "-i", "--input", required=True, help=f"type of mailbox to be bagged", choices=input_types, type=str.lower, nargs=None
+    "-i", "--input", required=True, help=f"The email export format to be packaged.", choices=input_types, type=str.lower, nargs=None
 )
 mailbagit_args.add_argument(
     "-d",
@@ -53,11 +128,21 @@ mailbagit_args.add_argument(
     choices=derivative_types,
     type=str.lower,
     required=False,
-    help=f"types of derivatives to create before bagging",
+    help=f"types of derivative formats to create during packaging",
     nargs="+",
+    widget="Listbox",
 )
 
 # add mailbag-specific optional args here
+mailbagit_options.add_argument("--css", help="Path to a CSS file to customize PDF derivatives.", nargs=None)
+mailbagit_options.add_argument(
+    "-c", "--compress", help="Compress the mailbag as ZIP, TAR, or TAR.GZ", nargs=None, choices=["tar", "zip", "tar.gz"]
+)
+mailbagit_options.add_argument(
+    "-r", "--dry_run", help="A dry run performs a trial run with no changes made", default=False, action="store_true"
+)
+# Yet-to-be-implemented:
+"""
 mailbagit_options.add_argument("--imap_host", help="the host for creating a mailbag from an IMAP connection", nargs=None)
 mailbagit_options.add_argument("--imap_password", help="password for creating a mailbag from an IMAP connection", nargs=None)
 mailbagit_options.add_argument("--exclude_folders", help="email folders to be excluded from the mailbag", nargs="+")
@@ -80,12 +165,7 @@ mailbagit_options.add_argument(
     action="store_true",
 )
 mailbagit_options.add_argument("-n", "--no-headers", help="will not include email headers in mailbag.csv", action="store_true")
-mailbagit_options.add_argument("--css", help="Path to a CSS file to customize PDF derivatives.", nargs=None)
-mailbagit_options.add_argument(
-    "-c", "--compress", help="Compress the mailbag as ZIP, TAR, or TAR.GZ", nargs=None, choices=["tar", "zip", "tar.gz"]
-)
-mailbagit_options.add_argument("-r", "--dry_run", help="Dry run", default=False, action="store_true")
-mailbagit_options.add_argument("-m", "--mailbag_name", required=True, help="Mailbag name", nargs=None)
+"""
 
 # Optional user-supplied mailbag metadata
 mailbagit_metadata.add_argument(
@@ -113,23 +193,40 @@ def gui():
 
 
 def main():
-    args = bagit_parser.parse_args()
+    args = mailbag_parser.parse_args()
     args.input = args.input.lower()
-    if args.input not in EmailAccount.registry.keys():
-        log.error("No parser found")
-        exit()
 
-    log.debug("Arguments:", args=args)
+    # handle arg errors
+    if args.input not in EmailAccount.registry.keys():
+        error_msg = 'Invalid derivatives, choose from: "' + '", "'.join(EmailAccount.registry.keys()) + '"'
+        bagit_parser.error((error_msg))
+
+    if isinstance(args.derivatives, str):
+        args.derivatives = args.derivatives.split(" ")
+        if not all(elem in derivative_types for elem in args.derivatives):
+            error_msg = 'Invalid derivatives, choose from: "' + '", "'.join(derivative_types) + '"'
+            bagit_parser.error((error_msg))
+
+    if args.input in args.derivatives:
+        error_msg = "Invalid derivatives, mailbagit does not support the source format as a derivative."
+        bagit_parser.error((error_msg))
+
+    if args.processes < 1:
+        error_msg = "processes must be valid integer > 0"
+        bagit_parser.error((error_msg))
 
     # Raise and error and exit when given multiple inputs
     if len(args.directory) > 1:
-        log.error(
-            "Multiple input paths provided. Mailbagit only supports single input paths. \
-            You may want to try providing a directory of email or running the command multiple \
-            times to create multiple mailbags."
+        error_msg = (
+            "Multiple input paths provided. Mailbagit only supports packaging single input paths. "
+            "You may want to try providing a directory of email or running the command multiple "
+            "times to create multiple mailbags."
         )
-        exit()
-    else:
-        args.directory = args.directory[0]
-        c = Controller(args)
-        return c.generate_mailbag()
+        bagit_parser.error((error_msg))
+
+    # Okay, if you made it here, args are good!
+    log.debug("Arguments:", args=args)
+
+    args.directory = args.directory[0]
+    c = Controller(args)
+    return c.generate_mailbag()
