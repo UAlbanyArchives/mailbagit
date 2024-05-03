6e2ea538082f6e68f8a5ff03890b3e6d0bf6355d
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2024-05-03 13:06:40 -0400

    bumped version again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mailbagit",
-    version="0.7.2",
+    version="0.7.3",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="A tool for preserving email in multiple preservation formats.",
